19413a26297e241e16d5e5cf70f787ed6bc82453
alyn <howerll12@gmail.com>
2022-01-12 17:22:45 +0900

    make question&answer function

--- 2.Pybo/pybo/views/main_views.py
+++ 2.Pybo/pybo/views/main_views.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for
+from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
@@ -10,4 +11,8 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    return 'Pybo index'
+    return redirect(url_for('question._list'))
+    #question : 등록된 블루프린트 이름
+    #_list : 블루프린트에 등록된 함수 
+    #url_for : 라우트가 설정된 함수명으로 URL을 찾아준다.
+
